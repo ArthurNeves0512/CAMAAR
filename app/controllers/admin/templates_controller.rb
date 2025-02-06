504936a325d04ef6a86c92362074238567ca7a64
Arthur <arthur@192.168.1.9>
2025-02-06 19:52:31 -0300

    roteamento para editar e criar template

--- app/controllers/admin/templates_controller.rb
+++ app/controllers/admin/templates_controller.rb
@@ -5,7 +5,16 @@ class Admin::TemplatesController < ApplicationController
   def index
     @templates = Template.all
   end
-
+  def edit
+    @template = Template.find(params[:id])
+    render :edit
+  end
+  def new
+    render :new
+  end
+  def destroy
+    @template = Template.find(params[:id])
+  end
   def show
     @template = Template.find(params[:id])
   end
