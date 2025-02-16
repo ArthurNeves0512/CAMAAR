b24c708b5e3d65a8ea4c6e2636ecd7df8e3ac814
teusdev <221000080@aluno.unb.br>
2025-02-16 20:10:22 -0300

    fix some file names and isolated testing rspec files into fixtures folder

--- spec/features/enviarFormularios_spec.rb
+++ spec/features/enviarFormularios_spec.rb
@@ -1,17 +1,17 @@
 require "rails_helper"
 
-RSpec.feature "Importar dados do SIGAA", type: :feature do
+RSpec.feature "Enviar formulários", type: :feature do
   background do
     # Criar um usuário admin
     user_adm = User.create(nome: "Janilson", email: "adm1@example.com", matricula: "2110320991", password: "senha123", password_confirmation: "senha123", role: "admin")
-    
+
     # Criar templates e turmas fictícias
     template1 = Template.create(name: "Template 1")
     template2 = Template.create(name: "Template 2")
-    
+
     classroom1 = Classroom.create(code: "A101", semester: "2021/2")
     classroom2 = Classroom.create(code: "B202", semester: "2021/2")
-    
+
     visit root_path
     expect(page).to have_content("Bem-vindo ao CAMAAR")
     click_link "Entrar"
@@ -29,28 +29,26 @@ RSpec.feature "Importar dados do SIGAA", type: :feature do
     expect(page).to have_button('Enviar Formulários')
     click_button('Enviar Formulários')
 
-    
+
     expect(page).to have_selector("#modal", visible: true)
-  
+
     # Preencher o nome do questionário
     fill_in "Digite o nome do questionário", with: "Questionário de Teste"
-  
+
     # Selecionar um template
     select 'Template 1', from: 'template_id', match: :first
-  
+
     # Selecionar as turmas
     within find_all('div.flex.items-center.gap-2', text: 'Estudos Em').first do
       all('input[type="checkbox"]').each(&:click)
     end
-  
+
     # Enviar o formulário
     puts find_button("Enviar")[:disabled] # Se for 'true', significa que está desabilitado
 
     click_button "Enviar"
-   
-    # Verificar se o sucesso foi exibido
-    #expect(find("#modal")).to have_content("✅ Formulários criados com sucesso!", wait: 30)
 
+    # Verificar se o sucesso foi exibido
+    # expect(find("#modal")).to have_content("✅ Formulários criados com sucesso!", wait: 30)
   end
-  
 end
