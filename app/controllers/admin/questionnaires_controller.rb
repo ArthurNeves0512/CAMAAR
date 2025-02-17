f7bb76b64aad939afdfcd1c611542fa451820717
teusdev <221000080@aluno.unb.br>
2025-02-17 01:27:40 -0300

    features 2

--- app/controllers/admin/questionnaires_controller.rb
+++ app/controllers/admin/questionnaires_controller.rb
@@ -1,9 +1,9 @@
 class Admin::QuestionnairesController < ApplicationController
     before_action :authenticate_user!
     before_action :authorize_admin!
-    
+
     protect_from_forgery with: :exception
-    
+
     def index
       @questionnaires = Questionnaire.all
       @templates = Template.all
@@ -14,35 +14,28 @@ class Admin::QuestionnairesController < ApplicationController
         classrooms: @classrooms
       }
     end
-    
+
     def create
       template_id = params[:template_id]
       classroom_codes = params[:classroom_codes]
-      # Se classroom_codes não for um Array, define como array vazio
       classroom_codes = classroom_codes.is_a?(Array) ? classroom_codes : []
       questionnaire_name = params[:name]
-      
+
       @questionnaire = Questionnaire.new(
         name: questionnaire_name,
         template_id: template_id,
         classroom_info: classroom_codes.join(",")
       )
-      
+
       if @questionnaire.save
-        classroom_codes.each do |code|
-          # Aqui você pode associar as turmas, se necessário
-        end
-        flash[:notice] = "✅ Formulários criados com sucesso!"
-        render json: { success: true }
+        redirect_to admin_root_path, notice: "✅ Formulários criados com sucesso."
       else
-        flash[:alert] = "❌ Erro ao criar formulário"
-        render json: { success: false, message: flash[:alert] }
+        redirect_to admin_root_path, alert: "❌ Erro ao criar formulário."
       end
     end
-    
+
     def show
       @questionnaire = Questionnaire.find(params[:id])
       render json: @questionnaire
     end
-  end
-  
+end
